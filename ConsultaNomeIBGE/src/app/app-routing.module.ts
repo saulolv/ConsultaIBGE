2f3229fa6517c1b51352dc162848dd5f00a561c3
Saulo <saulosantos229@gmail.com>
2023-08-18 18:01:56 -0300

    Fix bugs e adicionando a rota para o componente

--- ConsultaNomeIBGE/src/app/app-routing.module.ts
+++ ConsultaNomeIBGE/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultaNomesComponent } from './consulta-nomes/consulta-nomes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ConsultaNomesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
