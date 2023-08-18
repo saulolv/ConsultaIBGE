16db6c0365335298ab28365c2ee9985078f0ba6d
Saulo <saulosantos229@gmail.com>
2023-08-18 17:53:28 -0300

    Fix bugs

--- ConsultaNomeIBGE/src/app/app.module.ts
+++ ConsultaNomeIBGE/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,8 @@ import { ConsultaNomesComponent } from './consulta-nomes/consulta-nomes.componen
     ConsultaNomesComponent
   ],
   imports: [
+    BrowserModule,
+    FormsModule,
     HttpClientModule,
     BrowserModule,
     AppRoutingModule
